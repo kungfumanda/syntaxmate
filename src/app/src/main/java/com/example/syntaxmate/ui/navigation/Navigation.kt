864889bffd2edbcca56b40104411d59778606d6e
Amanda Costa <ansc@cin.ufpe.br>
2025-04-02 23:02:38 -0300

    feat: initialize database usage within activity

--- src/app/src/main/java/com/example/syntaxmate/ui/navigation/Navigation.kt
+++ src/app/src/main/java/com/example/syntaxmate/ui/navigation/Navigation.kt
@@ -10,10 +10,12 @@ import androidx.navigation.compose.composable
 import com.example.syntaxmate.ui.screens.FavoritesScreen
 import com.example.syntaxmate.ui.screens.HistoryScreen
 import com.example.syntaxmate.ui.screens.SearchScreen
+import com.example.syntaxmate.viewmodel.LanguageViewModel
 
 
 @Composable
-fun AppNavigation(navController: NavHostController) {
+fun AppNavigation(navController: NavHostController, languageViewModel: LanguageViewModel) {
+
    Scaffold (
        bottomBar = { BottomNavigationBar(navController) }
    ) { innerPadding -> NavHost(
@@ -21,7 +23,7 @@ fun AppNavigation(navController: NavHostController) {
        startDestination = "search",
        modifier = Modifier.padding(innerPadding)
    ) {
-        composable("search") { SearchScreen(navController) }
+        composable("search") { SearchScreen(navController, languageViewModel) }
         composable("favorites") { FavoritesScreen(navController) }
         composable("history") { HistoryScreen(navController) }
     } }
--- src/app/src/main/java/com/example/syntaxmate/ui/screens/SearchScreen.kt
+++ src/app/src/main/java/com/example/syntaxmate/ui/screens/SearchScreen.kt
@@ -2,18 +2,29 @@ package com.example.syntaxmate.ui.screens
 
 // Futura tela de busca
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.syntaxmate.ui.components.LanguageCard
+import com.example.syntaxmate.ui.components.LanguageCardSize
 import com.example.syntaxmate.ui.navigation.Routes
+import com.example.syntaxmate.viewmodel.LanguageViewModel
 
 @Composable
-fun SearchScreen(navController: NavController) {
+fun SearchScreen(navController: NavController, languageViewModel: LanguageViewModel) {
+
+    val languages = languageViewModel.languages.collectAsState(initial= emptyList())
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -24,6 +35,23 @@ fun SearchScreen(navController: NavController) {
         Text(text = "Search Screen")
 
         Spacer(modifier = Modifier.height(20.dp))
+
+        LazyColumn {
+            itemsIndexed(languages.value) { index, language ->
+                LanguageCard(
+                    languageName = language.name,
+                    isFavorite = language.isFavorite,
+                    sizeType = LanguageCardSize.COMPACT,
+                    onFavoriteClick = { languageViewModel.toggleFavorite(language) },
+                    onSelect = {
+                        Log.d("SearchScreen", "Selected: ${language.name}")
+                    }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
 
         // Tô deixando aqui mesmo tendo feito a barra inferior ja pra sabermos como fazer (:
         Button(
