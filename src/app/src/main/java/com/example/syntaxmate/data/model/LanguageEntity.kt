1c495b73503d8d1859a8edc4bb2282664ed4bfb0
Amanda Costa <ansc@cin.ufpe.br>
2025-04-09 15:48:46 -0300

    refactor: update LanguageEntity to use more info on the languages

--- src/app/src/main/java/com/example/syntaxmate/data/model/LanguageEntity.kt
+++ src/app/src/main/java/com/example/syntaxmate/data/model/LanguageEntity.kt
@@ -8,4 +8,13 @@ data class LanguageEntity(
     @PrimaryKey val name: String,
     val icon: String,
     val isFavorite: Boolean = false,
+    val paradigm: String,
+    val typing: String,
+    val examples: String
+    // val examples: List<CodeExampleJson>
+)
+
+data class CodeExample(
+    val title: String,
+    val code: String
 )
