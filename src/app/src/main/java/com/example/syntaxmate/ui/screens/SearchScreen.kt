843373337ff868feb6e50ef29a4bc985c7e89662
Amanda Costa <ansc@cin.ufpe.br>
2025-04-08 22:39:10 -0300

    feat: add initial homescreen structure with select languages feature

--- src/app/src/main/java/com/example/syntaxmate/ui/screens/SearchScreen.kt
+++ src/app/src/main/java/com/example/syntaxmate/ui/screens/SearchScreen.kt
@@ -4,69 +4,150 @@ package com.example.syntaxmate.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.syntaxmate.ui.components.LanguageCard
-import com.example.syntaxmate.ui.components.LanguageCardSize
-import com.example.syntaxmate.ui.navigation.Routes
+import com.example.syntaxmate.ui.components.*
 import com.example.syntaxmate.viewmodel.LanguageViewModel
 
 @Composable
 fun SearchScreen(navController: NavController, languageViewModel: LanguageViewModel) {
 
-    val languages = languageViewModel.languages.collectAsState(initial= emptyList())
-
+    val languages by languageViewModel.languages.collectAsState(initial= emptyList())
+    val selectedLanguages by languageViewModel.selectedLanguages.collectAsState(initial = emptyList())
+    var languageSearch by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(16.dp)
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Search Screen")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(
+                text = "Programming Languages",
+                style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier = Modifier.height(20.dp))
 
-        Spacer(modifier = Modifier.height(20.dp))
+            // Barra de buscas
+            OutlinedTextField(
+                value = languageSearch,
+                onValueChange = { languageSearch = it },
+                label = { Text("Search language") },
+                modifier = Modifier
+                    .width(160.dp)
+                    .padding(start = 8.dp),
+                singleLine = true
 
-        LazyColumn {
-            itemsIndexed(languages.value) { index, language ->
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Linguagens disponíveis
+        LazyRow(modifier = Modifier.padding(vertical = 8.dp)) {
+            val filtered = languages
+                .sortedByDescending { it.isFavorite }
+                .filter { it.name.contains(languageSearch, ignoreCase = true) }
+
+            items(filtered) { language ->
                 LanguageCard(
                     languageName = language.name,
                     isFavorite = language.isFavorite,
-                    sizeType = LanguageCardSize.COMPACT,
+                    isSelected = selectedLanguages.any { it.name == language.name },
+                    sizeType = LanguageCardSize.LARGE,
                     onFavoriteClick = { languageViewModel.toggleFavorite(language) },
                     onSelect = {
-                        Log.d("SearchScreen", "Selected: ${language.name}")
-                    }
+                        if (selectedLanguages.none { it.name == language.name }) {
+                            languageViewModel.toggleSelected(language) } }
                 )
+                Spacer(modifier = Modifier.width(8.dp))
             }
         }
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
+        // Linguagens selecionadas
+        if (selectedLanguages.isNotEmpty()) {
+            Text(text = "Selected Languages", style = MaterialTheme.typography.titleSmall)
 
-        // Tô deixando aqui mesmo tendo feito a barra inferior ja pra sabermos como fazer (:
-        Button(
-            onClick = { navController.navigate(Routes.Favorites.route) },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text(text = "Go to Favorites")
+            LazyRow(modifier = Modifier.padding(vertical = 8.dp)) {
+                items(selectedLanguages) { language ->
+                    LanguageCard(
+                        languageName = language.name,
+                        isFavorite = language.isFavorite,
+                        isSelected = true,
+                        sizeType = LanguageCardSize.COMPACT,
+                        onFavoriteClick = { languageViewModel.toggleFavorite(language) },
+                        onRemoveClick = { languageViewModel.toggleSelected(language) },
+                        onSelect = {
+                            Log.d("HomeScreen", "Selected: ${language.name}")
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
         }
 
-        Button(
-            onClick = { navController.navigate(Routes.History.route) },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text(text = "Go to History")
-        }
+        HorizontalDivider(
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            thickness = 1.dp,
+            modifier = Modifier.height(24.dp)
+        )
+
+        // Área para busca de sintaxe (estrutura base)
+        Text(text = "Syntax Search", style = MaterialTheme.typography.titleMedium)
+        OutlinedTextField(
+            value = "", // Adicionar estado para essa busca também
+            onValueChange = {},
+            label = { Text("Search syntax") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Inserir resultados depois
+
+
+
+
+//        LazyColumn {
+//            itemsIndexed(languages.value) { index, language ->
+//                LanguageCard(
+//                    languageName = language.name,
+//                    isFavorite = language.isFavorite,
+//                    sizeType = LanguageCardSize.COMPACT,
+//                    onFavoriteClick = { languageViewModel.toggleFavorite(language) },
+//                    onSelect = {
+//                        Log.d("SearchScreen", "Selected: ${language.name}")
+//                    }
+//                )
+//            }
+//        }
+//
+//        Spacer(modifier = Modifier.height(20.dp))
+//
+//
+//        // Tô deixando aqui mesmo tendo feito a barra inferior ja pra sabermos como fazer (:
+//        Button(
+//            onClick = { navController.navigate(Routes.Favorites.route) },
+//            modifier = Modifier.padding(top = 16.dp)
+//        ) {
+//            Text(text = "Go to Favorites")
+//        }
+//
+//        Button(
+//            onClick = { navController.navigate(Routes.History.route) },
+//            modifier = Modifier.padding(top = 16.dp)
+//        ) {
+//            Text(text = "Go to History")
+//        }
 
     }
 }
